2e52779c7fd9746131462fbcf85d9a558196e549
Benoît CORTIER <benoit.cortier@fried-world.eu>
2019-01-09 20:39:42 -0500

    Run clang format

--- include/wfc/utility/thread_manipulation.hpp
+++ include/wfc/utility/thread_manipulation.hpp
@@ -7,12 +7,13 @@ namespace wfc
 {
 	namespace details
 	{
-		inline std::size_t get_thread_id() {
+		inline std::size_t get_thread_id()
+		{
 			static std::atomic<std::size_t> i = 0;
 			thread_local static std::size_t id = i++;
 
 			return id;
 		}
-	}
+	} // namespace details
 } // namespace wfc
 #endif //WAITFREEHASHMAP_THREAD_MANIPULATION_HPP
